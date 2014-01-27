7ae430278f5711f3e2b2840c6a1b7cef1795b321
Cédric M <me.cedric@gmail.com>
2014-01-27 12:18:20 +0100

    Solution Exo 01 Interacting with code

--- 01.Interacting_with_code/main.rb
+++ 01.Interacting_with_code/main.rb
@@ -5,21 +5,26 @@ database_path = "db/jukebox.sqlite"
 db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db,table_name)
-  # your code here
+  db.execute("SELECT COUNT(*) FROM #{table_name}")
 end
 
-puts "There are #{number_of_rows(db,"Artist")} artists"
-puts "There are #{number_of_rows(db,"Track")} tracks"
-puts "There are #{number_of_rows(db,"Album")} albums"
-puts "There are #{number_of_rows(db,"Genre")} genres"
-puts "There are #{number_of_rows(db,"MediaType")} media types"
+
+puts "There are #{number_of_rows(db,"Artist").join} artists"
+puts "There are #{number_of_rows(db,"Track").join} tracks"
+puts "There are #{number_of_rows(db,"Album").join} albums"
+puts "There are #{number_of_rows(db,"Genre").join} genres"
+puts "There are #{number_of_rows(db,"MediaType").join} media types"
 
 
 # returns all the artists sorted by alphabetical order.
-artists = # your code here
+artists = db.execute("SELECT name FROM Artist ORDER BY Name")
+puts artists
 
 # returns all the love songs
-love_songs = # your code here
+love_songs_list = db.execute("SELECT Name FROM Track WHERE Name LIKE '%love%' ESCAPE '#' ORDER BY Name")
+puts love_songs_list
+
 
 # returns all the tracks that are longer than 5 minutes.
-long_tracks = # your code here
+long_tracks = db.execute("SELECT name FROM Track WHERE Milliseconds > 700000 ORDER BY Name")
+puts long_tracks

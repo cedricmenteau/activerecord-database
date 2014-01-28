f2aaa281e70698b81c0c871eca8f1af825cc85e8
Cédric M <me.cedric@gmail.com>
2014-01-28 16:10:35 +0100

    Solution 05 Cookbook

--- 05.CookBook/config/application.rb
+++ 05.CookBook/config/application.rb
@@ -4,14 +4,16 @@ require 'active_record'
 ActiveRecord::Base.configurations = YAML::load(IO.read(File.join(File.dirname(__FILE__),'database.yml')))
 ActiveRecord::Base.establish_connection('development')
 
-
 # Migrations
 # Create the migration for the Recipe table
 unless ActiveRecord::Base.connection.tables.include? "recipes"
   ActiveRecord::Schema.define do
-    create_table :recipes do |t|
+    create_table :recipes do |r|
       # your code here to create the schema of the Recipes table  
-       
+      r.string :name 
+      r.string :description
+	  r.integer :length
+	  r.integer :difficulty
     end
   end
 end
--- 05.CookBook/cookbook.rb
+++ 05.CookBook/cookbook.rb
@@ -3,20 +3,34 @@ require './models/recipe'
 
 # your program here
 
-puts "Salut Robuchon, what do you want to do today?"
-puts "1. create a recipe"
-puts "2. delete all recipes"
-puts "3. read your recipes"
+while true
 
-choice = gets.chomp.to_i
+	puts "Salut Robuchon, what do you want to do today?"
+	puts "1. create a recipe"
+	puts "2. delete all recipes"
+	puts "3. read your recipes"
+	puts "4. Exit"
 
-if choice == 1
-  
-elsif choice == 2
-  
-elsif choice == 3
-  
-else
-  puts "i did not understand"
-  
+	choice = gets.chomp.to_i
+
+	break if choice == 4
+	if choice == 1
+	  puts "Please input the name of your recipe"
+	  name = gets.chomp
+	  puts "Please input its description"
+	  description = gets.chomp
+	  puts "Please input its length"
+	  length = gets.chomp.to_i
+	  puts "Please input its difficulty"
+	  difficulty = gets.chomp.to_i             
+	  Recipe.create(name: name, description: description, length: length, difficulty: difficulty)
+	elsif choice == 2
+	  Recipe.all.each {|row| row.destroy}
+	elsif choice == 3
+	  Recipe.all.each do |recipe|
+	  puts "#{recipe.name} #{recipe.description} #{recipe.length} #{recipe.difficulty}"
+	end
+	else
+	  puts "i did not understand"
+	end
 end

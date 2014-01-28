c53a15795f76b815d250422d70792d5073e218c1
Cédric M <me.cedric@gmail.com>
2014-01-28 11:51:53 +0100

    Solution Exo 04 CRUD

--- 04.CRUD/main.rb
+++ 04.CRUD/main.rb
@@ -6,35 +6,62 @@ db = SQLite3::Database.new(db_path)
 
 # creates the schema of the database
 # your code here
-sql = %q{
-  
+
+sql = %q{ CREATE TABLE IF NOT EXISTS Recipes (
+        Id integer PRIMARY KEY AUTOINCREMENT,
+        Name varchar(255) NOT NULL,
+        Description varchar(255),
+        Length int,
+        Difficulty int,
+        )  
 }
-db.execute(sql)
+
+begin
+  db.execute(sql)
+  puts "Welcome folks!"
+rescue
+  puts "You again? Welcome back!"
+end
 
 #*************************************
 
 # function to create a recipe
 def create_recipe(db,name,description,length,difficulty)
-  # your code here
+	db.execute("
+		INSERT INTO Recipes (Name, Description, Length, Difficulty)
+    VALUES ('#{name}','#{description}','#{length}','#{difficulty}')
+    ;")
 end
 
 # function to delete a recipe
 def delete_recipe(db,id)
-  # your code here
+  db.execute("
+  	DELETE FROM Recipes
+		WHERE Id = '#{id}'
+		;")
 end
 
 # function to delete all recipes
 def delete_all_recipes(db)
-  # your code here
+	db.execute("
+  DELETE FROM Recipes
+  ;")
 end
 
 # function to update a recipe
 def update_recipe(db,id,description)
-  # your code here
+	db.execute("
+  UPDATE Recipes
+	SET description = '#{description}'
+  WHERE Id = '#{id}'
+	;")
 end
 
+# function to get all recipes
 def get_recipes(db)
-  # function to get all recipes
+  db.execute("
+  SELECT * FROM Recipes
+  ;")
 end
 
 
@@ -42,24 +69,37 @@ end
 
 # program
 
+while true
+
 puts "Salut Robuchon, what do you want to do today?"
 puts "1. create a recipe"
 puts "2. delete all recipes"
 puts "3. read your recipes"
+puts "4. Exit"
 
 choice =  gets.chomp.to_i
 
-if choice == 1
-  # your code here to create a recipe
-  # you need to ask for name, description, length and difficulty
-  
-elsif choice == 2
-  # your code here to delete all recipes
-  
-elsif choice == 3
-  # your code here to read all recipes
-end 
-  
+break if choice == 4
+	if choice == 1
+	  # your code here to create a recipe
+	  # you need to ask for name, description, length and difficulty
+	  puts "Please input the name of your recipe"
+	  name = gets.chomp
+	  puts "Please input its description"
+	  description = gets.chomp
+	  puts "Please input its length"
+	  length = gets.chomp.to_i
+	  puts "Please input its difficulty"
+	  difficulty = gets.chomp.to_i             
+	  create_recipe(db, name, description, length, difficulty)
+	elsif choice == 2
+	  # your code here to delete all recipes
+	  delete_all_recipes(db)
+	elsif choice == 3
+	  # your code here to read all recipes
+	  get_recipes(db)
+	end 
+end
   
   
   

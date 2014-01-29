9a0bd63dd04754822162f9d27e7dc1039edd0925
Cédric M <me.cedric@gmail.com>
2014-01-29 17:02:17 +0100

    Solution Exo 07 Seed

--- 07.Seed/db/seed.rb
+++ 07.Seed/db/seed.rb
@@ -1,4 +1,6 @@
 require './models/recipe'
+require 'nokogiri'
+require 'open-uri'
 
 # Seed you database with recipe data from marmiton
 
@@ -9,4 +11,15 @@ ingredients = ["curry", "crevettes", "agneau", "pomme", "orange", "café", "aspe
 
 # Your code goes here 
 # 1. Scrape recipes data from marmiton for these ingredients
+sample.each do |ingredient|
+	html_file = open("http://www.marmiton.org/recettes/recherche.aspx?aqt=#{ingredient}")
+	recipes = Nokogiri::HTML(html_file)
 # 2. Create associated recipe records in the DB
+	recipes.search('.m_search_result').each do |element|
+		recipe_name = element.search('.m_search_titre_recette a').inner_text
+		recipe_description = element.search('.m_search_result_part4').inner_text
+		recipe_length = recipe_description.match(/(\d+)\smin/)[1].to_i
+		recipe_difficulty = rand(1..9)
+		Recipe.create(name: recipe_name, description: recipe_description, length: recipe_length, difficulty: recipe_difficulty)
+  end
+end                                             
